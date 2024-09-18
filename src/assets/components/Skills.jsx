344d495a97112383bc666c23c3ddecbf72e693ef
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2024-09-18 22:51:41 +0530

    added skills  component

---
+++ src/assets/components/Skills.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>
+         <h1 className="text-2xl font-bold">Skills</h1>
+            <ul className="list-disc ml-5 mt-2">
+              <li>C</li>
+              <li>Java</li>
+              <li>React</li>
+              <li>Database Management</li>
+              <li>Problem Solving</li>
+              <li>Javascript</li>
+            </ul>
+    </div>
+  )
+}
+
+export default Skills
