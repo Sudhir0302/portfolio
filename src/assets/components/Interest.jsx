6220529fe7d540736cb2eaf0a92d1b8f35485d0f
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2024-09-18 22:51:11 +0530

    added interest component

---
+++ src/assets/components/Interest.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Interest = () => {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold">Area of Interest</h1>
+            <ul className="list-disc ml-5 mt-2 ">
+              <li>Full Stack Development</li>
+              <li>Java Development</li>
+              <li>Web Development</li>
+              <li>Database Management Systems</li>
+            </ul>
+    </div>
+  )
+}
